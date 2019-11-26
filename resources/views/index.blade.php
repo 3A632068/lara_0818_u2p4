e24216583fe2d485a6da9465cc86eac27674f6d9
PengHueiSin <eason12326@gmail.com>
2019-11-26 14:30:49 +0800

    修改 index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,9 @@
 @extends('layouts.master')
+
 @section('title', 'Clean Blog')
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
+@section('content')
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Clean Blog</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    @section('content')
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
     <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
@@ -57,6 +25,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-preview">
+                    <!--<a href="post.html">-->
                     <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Man must explore, and this is exploration at its greatest
@@ -69,6 +38,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
+                    <!--<a href="post.html">-->
                     <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
@@ -78,6 +48,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
+                    <!--<a href="post.html">-->
                     <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Science has not yet mastered prophecy
@@ -90,6 +61,7 @@
                 </div>
                 <hr>
                 <div class="post-preview">
+                    <!--<a href="post.html">-->
                     <a href="{{route('posts.show')}}">
                         <h2 class="post-title">
                             Failure is not an option
@@ -110,8 +82,5 @@
             </div>
         </div>
     </div>
-    @endsection
 
-</body>
-
-</html>
+@endsection
